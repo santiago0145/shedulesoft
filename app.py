23ea15338321f91eb94e1d0053dd581b334a9ddc
Eddy Santiago Guillen Valencia <santiagoguillen.02@gmail.com>
2020-09-16 20:50:26 -0500

    se agrega ruta de registro usuario

--- app.py
+++ app.py
@@ -19,6 +19,19 @@ def insertar():
                 conexion.execute("INSERT INTO usuarios (nombres, correo, usuario, password) VALUES (%s, %s, %s,%s)", (nombre, correo, nombre_usuario, contrasena,))
                 mysql.connection.commit()
                 return redirect(url_for('login'))
+#registro usuario
+@app.route('/insert',methods = ['POST'])
+def insertar():
+        flash('Te has registrado correctamente!')
 
+        if request.method == "POST":
+                nombre = request.form['nombre']
+                correo = request.form['correo']
+                nombre_usuario = request.form['nombre_usuario']
+                contrasena = request.form['contrasena']
+                conexion = mysql.connection.cursor()
+                conexion.execute("INSERT INTO usuarios (nombres, correo, usuario, password) VALUES (%s, %s, %s,%s)", (nombre, correo, nombre_usuario, contrasena,))
+                mysql.connection.commit()
+                return redirect(url_for('login'))
 if __name__ == "__main__":
 	app.run(debug=True)
